efea9d17ccb5659d64dcef37f0bb6027b2b7811b
harshitbansal05 <harshit.bansalec@gmail.com>
2018-12-17 00:01:18 +0530

    add evaluation doe

--- src/main.py
+++ src/main.py
@@ -31,50 +31,25 @@ def main():
                                   weight_decay = ref.weightDecay, 
                                   momentum = ref.momentum)
 
-  if opt.ratio3D < ref.eps:
-    val_loader = torch.utils.data.DataLoader(
-        MPII(opt, 'val', returnMeta = True), 
-        batch_size = 1, 
-        shuffle = False,
-        num_workers = int(ref.nThreads)
-    )
-  else:
-    val_loader = torch.utils.data.DataLoader(
-        H36M(opt, 'val'), 
-        batch_size = 1, 
-        shuffle = False,
-        num_workers = int(ref.nThreads)
-    )
-  
-
-  if opt.test:
-    val(0, opt, val_loader, model, criterion)
-    return
-
-  train_loader = torch.utils.data.DataLoader(
-      Fusion(opt, 'train'), 
-      batch_size = opt.trainBatch, 
-      shuffle = True if opt.DEBUG == 0 else False,
-      num_workers = int(ref.nThreads)
+  val_loader = torch.utils.data.DataLoader(
+      H36M(opt, 'val'), 
+	  batch_size = 1, 
+	  shuffle = False,
+	  num_workers = int(ref.nThreads)
   )
+  
+  loss_val, acc_val, mpjpe_val, loss3d_val = val(0, opt, val_loader, model, criterion)
+  logger.scalar_summary('loss_val', loss_val, 1)
+  logger.scalar_summary('acc_val', acc_val, 1)
+  logger.scalar_summary('mpjpe_val', mpjpe_val, 1)
+  logger.scalar_summary('loss3d_val', loss3d_val, 1)
+  
+  logger.write('{:8f} {:8f} {:8f} {:8f} {:8f} {:8f} {:8f} {:8f} \n'.format(loss_val, acc_val, mpjpe_val, loss3d_val))
+  print('Loss Val: ', loss_val)
+  print('Accuracy Val: ', acc_val)
+  print('MPJPE Val: ', mpjpe_val)
+  print('Loss 3D Val: ', loss3d_val)
 
-  for epoch in range(1, opt.nEpochs + 1):
-    loss_train, acc_train, mpjpe_train, loss3d_train = train(epoch, opt, train_loader, model, criterion, optimizer)
-    logger.scalar_summary('loss_train', loss_train, epoch)
-    logger.scalar_summary('acc_train', acc_train, epoch)
-    logger.scalar_summary('mpjpe_train', mpjpe_train, epoch)
-    logger.scalar_summary('loss3d_train', loss3d_train, epoch)
-    if epoch % opt.valIntervals == 0:
-      loss_val, acc_val, mpjpe_val, loss3d_val = val(epoch, opt, val_loader, model, criterion)
-      logger.scalar_summary('loss_val', loss_val, epoch)
-      logger.scalar_summary('acc_val', acc_val, epoch)
-      logger.scalar_summary('mpjpe_val', mpjpe_val, epoch)
-      logger.scalar_summary('loss3d_val', loss3d_val, epoch)
-      torch.save(model, os.path.join(opt.saveDir, 'model_{}.pth'.format(epoch)))
-      logger.write('{:8f} {:8f} {:8f} {:8f} {:8f} {:8f} {:8f} {:8f} \n'.format(loss_train, acc_train, mpjpe_train, loss3d_train, loss_val, acc_val, mpjpe_val, loss3d_val))
-    else:
-      logger.write('{:8f} {:8f} {:8f} {:8f} \n'.format(loss_train, acc_train, mpjpe_train, loss3d_train))
-    adjust_learning_rate(optimizer, epoch, opt.dropLR, opt.LR)
   logger.close()
 
 if __name__ == '__main__':
